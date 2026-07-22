eb3797dfb4bcafe8ecda1510db60e1ff223d1482
anonymous Del Barco <61146153+maridasi47500@users.noreply.github.com>
2026-07-22 15:12:21 +0200

    wow dljfghsfj

---
+++ demofile.sh
@@ -0,0 +1,9 @@
+
+mkdir templates 
+python3 scaffold.py user username email password phone country_id
+python3 scaffold.py country name
+python3 scaffold.py cat name
+python3 scaffold.py photo country_id cat_id pic
+python3 scaffold.py songs country_id title artist music
+python3 scaffold.py likes user_id photo_id
+python3 scaffold.py likesongs user_id song_id
