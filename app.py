718f68c7665fe6bcfd6dfbaa095df6b887b7996e
anonymous Del Barco <61146153+maridasi47500@users.noreply.github.com>
2026-07-22 15:15:46 +0200

    wow dljfghsfj

--- app.py
+++ app.py
@@ -187,3 +187,94 @@ def add_one_likesongs():
     one_user = query_db("select * from likesongs limit 1", one=True)
     return render_template("likesongsform.html", likesongss=user, one_user=one_user, the_title="add new likesongs")
 
+@app.route("/add_one_user", methods=["GET","POST"])
+def add_one_user():
+
+    if request.method == 'POST':
+
+        the_username = "anonyme"
+        one_user = query_db("insert into user (username,email,password,phone,country_id) values (:username,:email,:password,:phone,:country_id)",request.form)
+        user = query_db('select * from user')
+        return render_template("userform.html", users=user, one_user=one_user, the_title="add new user")
+    user = query_db('select * from user')
+    one_user = query_db("select * from user limit 1", one=True)
+    return render_template("userform.html", users=user, one_user=one_user, the_title="add new user")
+
+@app.route("/add_one_country", methods=["GET","POST"])
+def add_one_country():
+
+    if request.method == 'POST':
+
+        the_username = "anonyme"
+        one_user = query_db("insert into country (name) values (:name)",request.form)
+        user = query_db('select * from country')
+        return render_template("countryform.html", countrys=user, one_user=one_user, the_title="add new country")
+    user = query_db('select * from country')
+    one_user = query_db("select * from country limit 1", one=True)
+    return render_template("countryform.html", countrys=user, one_user=one_user, the_title="add new country")
+
+@app.route("/add_one_cat", methods=["GET","POST"])
+def add_one_cat():
+
+    if request.method == 'POST':
+
+        the_username = "anonyme"
+        one_user = query_db("insert into cat (name) values (:name)",request.form)
+        user = query_db('select * from cat')
+        return render_template("catform.html", cats=user, one_user=one_user, the_title="add new cat")
+    user = query_db('select * from cat')
+    one_user = query_db("select * from cat limit 1", one=True)
+    return render_template("catform.html", cats=user, one_user=one_user, the_title="add new cat")
+
+@app.route("/add_one_photo", methods=["GET","POST"])
+def add_one_photo():
+
+    if request.method == 'POST':
+
+        the_username = "anonyme"
+        one_user = query_db("insert into photo (country_id,cat_id,pic) values (:country_id,:cat_id,:pic)",request.form)
+        user = query_db('select * from photo')
+        return render_template("photoform.html", photos=user, one_user=one_user, the_title="add new photo")
+    user = query_db('select * from photo')
+    one_user = query_db("select * from photo limit 1", one=True)
+    return render_template("photoform.html", photos=user, one_user=one_user, the_title="add new photo")
+
+@app.route("/add_one_songs", methods=["GET","POST"])
+def add_one_songs():
+
+    if request.method == 'POST':
+
+        the_username = "anonyme"
+        one_user = query_db("insert into songs (country_id,title,artist,music) values (:country_id,:title,:artist,:music)",request.form)
+        user = query_db('select * from songs')
+        return render_template("songsform.html", songss=user, one_user=one_user, the_title="add new songs")
+    user = query_db('select * from songs')
+    one_user = query_db("select * from songs limit 1", one=True)
+    return render_template("songsform.html", songss=user, one_user=one_user, the_title="add new songs")
+
+@app.route("/add_one_likes", methods=["GET","POST"])
+def add_one_likes():
+
+    if request.method == 'POST':
+
+        the_username = "anonyme"
+        one_user = query_db("insert into likes (user_id,photo_id) values (:user_id,:photo_id)",request.form)
+        user = query_db('select * from likes')
+        return render_template("likesform.html", likess=user, one_user=one_user, the_title="add new likes")
+    user = query_db('select * from likes')
+    one_user = query_db("select * from likes limit 1", one=True)
+    return render_template("likesform.html", likess=user, one_user=one_user, the_title="add new likes")
+
+@app.route("/add_one_likesongs", methods=["GET","POST"])
+def add_one_likesongs():
+
+    if request.method == 'POST':
+
+        the_username = "anonyme"
+        one_user = query_db("insert into likesongs (user_id,song_id) values (:user_id,:song_id)",request.form)
+        user = query_db('select * from likesongs')
+        return render_template("likesongsform.html", likesongss=user, one_user=one_user, the_title="add new likesongs")
+    user = query_db('select * from likesongs')
+    one_user = query_db("select * from likesongs limit 1", one=True)
+    return render_template("likesongsform.html", likesongss=user, one_user=one_user, the_title="add new likesongs")
+
--- schema.sql
+++ schema.sql
@@ -76,3 +76,42 @@ create table if not exists likesongs(
         user_id text,
             song_id text
                     );
+create table if not exists user(
+        id integer primary key autoincrement,
+        username text,
+            email text,
+            password text,
+            phone text,
+            country_id text
+                    );
+create table if not exists country(
+        id integer primary key autoincrement,
+        name text
+                    );
+create table if not exists cat(
+        id integer primary key autoincrement,
+        name text
+                    );
+create table if not exists photo(
+        id integer primary key autoincrement,
+        country_id text,
+            cat_id text,
+            pic text
+                    );
+create table if not exists songs(
+        id integer primary key autoincrement,
+        country_id text,
+            title text,
+            artist text,
+            music text
+                    );
+create table if not exists likes(
+        id integer primary key autoincrement,
+        user_id text,
+            photo_id text
+                    );
+create table if not exists likesongs(
+        id integer primary key autoincrement,
+        user_id text,
+            song_id text
+                    );
